9c2286eabd00420ba3754cec3b17f9447894df56
Joe Smith <jsmith0684@gmail.com>
2023-03-16 21:58:13 -0700

    Code cleanup.

--- linter.py
+++ linter.py
@@ -6,14 +6,11 @@ from SublimeLinter.lint import (
 
 
 class Revive(Linter):
-    # cmd = "revive -formatter ndjson ${args}"
     cmd = ("revive", "-formatter", "ndjson", "${args}", "${file}")
-    # regex = r"^.+:(?P<line>\d+):(?P<col>\d+):\s+(?P<message>.+)"
     regex = (
         r".+\"Severity\":\"((?P<error>error)|(?P<warning>warning))\".+?\"Failure\":\"(?P<message>.*?)\".+?"
         '"Offset":(?P<col>\d+).+?"Line":(?P<line>\d+)'
     )
-    # regex = r".+\"Severity\":\"((?P<error>error)|(?P<warning>warning))\".+?\"Failure\":\"(?P<message>.*?)\".+?\"Offset\":(?P<col>\d+).+?\"Line\":(?P<line>\d+)"
     multiline = False
     default_type = WARNING
     tempfile_suffix = "go"
